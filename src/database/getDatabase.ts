7bf68cc2ef7a60d0a7d224da0de691ef497a348e
kualta <contact@kualta.dev>
2024-10-07 19:48:33 +0400

    update study schema

--- src/database/getDatabase.ts
+++ src/database/getDatabase.ts
@@ -1,5 +1,5 @@
 import { init } from "@instantdb/core";
-import type { SakuraSchema } from "./schema";
+import type { StudyTracker } from "./schema";
 
 const APP_ID = process.env.APP_ID;
 
@@ -7,4 +7,4 @@ if (!APP_ID) {
 	throw new Error("APP_ID is not set, cannot initialize database");
 }
 
-export const db = init<SakuraSchema>({ appId: APP_ID });
+export const db = init<StudyTracker>({ appId: APP_ID });
